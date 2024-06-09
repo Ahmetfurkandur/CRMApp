2fcae2c2448973a2d67644ed0bc65bc1c7b0d168
ahmetfurkandur <fkn3388@hotmail.com>
2024-06-09 22:12:05 +0300

    Add Customer.cs, Modify Contact.cs

--- src/Domain/Entities/Contact.cs
+++ src/Domain/Entities/Contact.cs
@@ -8,16 +8,12 @@ namespace Domain.Entities
 {
     public class Contact : BaseEntity
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Email { get; set; }
-        public string Role { get; set; }
-        public string Company { get; set; }
         public string PhoneNumber { get; set; }
-        
         public string WebsiteUrl { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string ZipCode { get; set; }
+        public string Country { get; set; }
     }
 }
