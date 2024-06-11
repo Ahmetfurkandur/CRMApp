3e69f7a8f8fd705df0ecb51a4d82071e53142fdd
ahmetfurkandur <fkn3388@hotmail.com>
2024-06-11 22:21:02 +0300

    Add CrmAppDbContext, Configuration, ServiceRegistration. Modify Persistence.csproj, appsettings.Development.json

---
+++ src/Persistence/Contexts/CrmAppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task = Domain.Entities.Task;
+
+namespace Persistence.Contexts
+{
+    public class CrmAppDbContext : DbContext
+    {
+        public CrmAppDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Deal> Deals { get; set; }
+        public DbSet<PotentialCustomer> PotentialCustomers { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ src/Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<CrmAppDbContext>(options => options.UseSqlServer(Configuration.ConnectionString));
+        }
+    }
+}
