b76c5022f78d05e4bccaae47e78f6910f2269353
ahmetfurkandur <fkn3388@hotmail.com>
2024-06-12 23:43:31 +0300

    Add Base Repository Interfaces and first migration

---
+++ src/Application/Repositories/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll(bool tracking = true);
+        IQueryable<T> GetWhere(Action<Func<T, bool>> filter, bool tracking = true);
+
+        Task<T> GetSingleAsync(Action<Func<T, bool>> filter, bool tracking = true);
+
+        Task<T> GetByIdAsync(Guid id, bool tracking = true);
+    }
+}
