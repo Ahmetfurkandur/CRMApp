faf611dd4ceb4a9bb784c58c0cd2008bdc611eee
ahmetfurkandur <fkn3388@hotmail.com>
2024-06-18 21:48:08 +0300

    Add SaveChangesAsync Intercaptor

--- src/Persistence/Contexts/CrmAppDbContext.cs
+++ src/Persistence/Contexts/CrmAppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,22 @@ namespace Persistence.Contexts
     {
         public CrmAppDbContext(DbContextOptions options) : base(options)
         { }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow
+                };
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
 
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Customer> Customers { get; set; }
