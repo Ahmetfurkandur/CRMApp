efe42aa405e72243e1d47627ad33906f39264320
ahmetfurkandur <fkn3388@hotmail.com>
2024-06-29 17:40:30 +0300

    Add Auto increment for Base Id

--- src/Domain/Entities/Common/BaseEntity.cs
+++ src/Domain/Entities/Common/BaseEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace Domain.Entities.Common
 {
     public class BaseEntity
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public Guid Id { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
