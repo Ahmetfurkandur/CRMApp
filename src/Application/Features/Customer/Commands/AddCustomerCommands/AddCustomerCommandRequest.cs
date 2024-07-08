d704dc558be925151b9ab7997df74de8ced0e570
ahmetfurkandur <fkn3388@hotmail.com>
2024-07-08 13:19:48 +0300

    ModifyAddCustomerCommandRequest

--- src/Application/Features/Customer/Commands/AddCustomerCommands/AddCustomerCommandRequest.cs
+++ src/Application/Features/Customer/Commands/AddCustomerCommands/AddCustomerCommandRequest.cs
@@ -9,5 +9,9 @@ namespace Application.Features.Customer.Commands.AddCustomerCommands
 {
     public record AddCustomerCommandRequest : IRequest<AddCustomerCommandResponse>
     {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Role { get; init; }
+        public string Company { get; init; }
     }
 }
